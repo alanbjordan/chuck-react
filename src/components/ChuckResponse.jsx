6d7dc10b57e66a9e0a5ba841185968bdc685ec76
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-24 23:47:33 -0400

    Created placeholder component for Chuck responses

---
+++ src/components/ChuckResponse.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ChuckResponse extends Component {
+  render() {
+    return (
+      <div className="header">
+        <p>"Give Me Some Data!!"</p>
+      </div>
+    );
+  }
+}
+
+export default ChuckResponse;
