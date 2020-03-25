36fb164fabbaa17572cdfdef0f99b865c4c1ecb3
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-24 23:52:26 -0400

    Created header div

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <p>Want Entertainment? Chuck's Got You Covered?</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
