f191c2944067cbf6715ccc54ef2d44898574afe2
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-26 12:06:56 -0400

    refactored and added props to component class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
+    const {hText} = this.props;
     return (
       <div className="header">
-        <p>Want Entertainment? Chuck's Got You Covered?</p>
+        <p>{hText}</p>
       </div>
     );
   }
