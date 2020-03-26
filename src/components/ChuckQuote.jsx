cca61f79c83aa05ef266675773fb35e722a367c0
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-26 18:40:24 -0400

    added component mount function and fetched the chuck norris api

--- src/components/ChuckQuote.jsx
+++ src/components/ChuckQuote.jsx
@@ -2,22 +2,34 @@ import React, { Component } from "react";
 
 class ChuckQuote extends Component {
   state = {
-    quote: "Requesting a Chuck Norris quote..."
+    quote: "Requesting a Chuck Norris quote...",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Chuck_Norris_May_2015.jpg/220px-Chuck_Norris_May_2015.jpg"
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        quote: "Chuck Norris threw a grenade and killed 50 people, then it exploded."
-      })
-    }, 3000);
-  }
+  async componentDidMount() {
+    try {
+        const response = await fetch('https://api.chucknorris.io/jokes/random?category=animal');
+        const data = await response.json();    
+        this.setState({
+          quote: data.value,
+          image: data.icon_url
+        })
+    } catch (error) {
+        this.setState({
+          quote: error.message,
+          image: error.message
+        })
+    }
+}
+
 
   render() {
     const { quote } = this.state;
+    const { image } = this.state;
     return (
       <div className="theQuote">
         <p>{quote}</p>
+        <img src={image}/>
       </div>
     );
   }
